a416b88fb555cab76693cbe35c7e14c2392cc4ca
slomo <steve.harrison@gmx.net>
2011-01-19 00:10:43 +0100

    first version of bootstrap skript

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+NODE_VERSION=0.3.5
+
+## Download Helper 
+# $1 - url 
+# $2 - tarball name
+download() {
+    echo $1
+    mkdir -p usr/src
+    
+    if [ ! -f ${2} ]; then
+        curl ${1} > ${2}
+    fi
+
+    tar -xf ${2} -C usr/src
+}
+
+# fetch and compile nodejs
+download http://nodejs.org/dist/node-v${NODE_VERSION}.tar.gz node-${NODE_VERSION}.tar.gz
+rm -f node-${NODE_VERSION}.tar.gz
+
+cd usr/src/node-v${NODE_VERSION} 
+./configure --prefix=../..
+make install
+cd ../../..
+
+# fetch and compile npm
+curl http://npmjs.org/install.sh |  PATH=`pwd`/usr/bin:${PATH} sh
