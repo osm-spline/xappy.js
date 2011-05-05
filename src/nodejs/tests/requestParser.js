ce0e3ed763c721e1c16648202d851c3bc761c51a
Alexander Sulfrian <alexander@sulfrian.net>
2011-05-06 01:17:33 +0200

    requestParser: port urlToResObj tests to async_testing (#12)

--- src/nodejs/tests/requestParser.js
+++ src/nodejs/tests/requestParser.js
@@ -175,6 +175,80 @@ module.exports = {
         });
         test.finish();
     },
+
+    // test all simple objects { node, way, relation }
+
+    'parse simple node string': function(test) {
+        test.deepEqual(parser.parse('/node'), {object: 'node'});
+        test.finish();
+    },
+
+    'parse simple node string with trailing slash': function(test) {
+        test.deepEqual(parser.parse('/node/'), {object: 'node'});
+        test.finish();
+    },
+
+    'parse simple way string': function(test) {
+        debugger;
+        var x = parser.parse('/way');
+        test.deepEqual(x, { object: 'way' });
+        test.finish();
+    },
+
+    'parse simple way string with trailing slash': function(test) {
+        test.deepEqual(parser.parse('/way/'), { object: 'way' });
+        test.finish();
+    },
+
+    'parse simple relation string': function(test) {
+        test.deepEqual(parser.parse('/relation'), { object: 'relation' });
+        test.finish();
+    },
+
+    'parse simple relation string with trailing slash': function(test) {
+        test.deepEqual(parser.parse('/relation/'), { object: 'relation' });
+        test.finish();
+    },
+
+    // TODO invalid object eg not (node, way, relation)
+
+    'parse node with bbox': function(test) {
+        test.deepEqual(parser.parse('/node[bbox=0,51.5,0.25,51.75]'),
+                       { object: 'node', bbox: {left:0, bottom:51.5, right:0.25, top:51.75} });
+        test.finish();
+    },
+
+    'parse node with simple tag': function(test) {
+        test.deepEqual(parser.parse('/node[key=value]'),
+                       { object: 'node', tag: {key: ['key'], value: ['value']} });
+        test.finish();
+    },
+
+    'parse node with tag with two values': function(test) {
+        test.deepEqual(parser.parse('/node[key=foo|bar]'),
+                       { object: 'node', tag: {key: ['key'], value: ['foo', 'bar']} });
+        test.finish();
+    },
+
+    'parse node with tag with two keys': function(test) {
+        test.deepEqual(parser.parse('/node[foo|bar=value]'),
+                       { object: 'node', tag: {key: ['foo', 'bar'], value: ['value']} });
+        test.finish();
+    },
+
+    'parse node with tag with cross product': function(test) {
+        test.deepEqual(parser.parse('/node[key1|key2=value1|value2]'),
+                       { object: 'node', tag: {key: ['key1', 'key2'], value: ['value1', 'value2']} });
+        test.finish();
+    },
+
+    'parse node with bbox and simple tag': function(test) {
+        test.deepEqual(parser.parse('/node[bbox=0,0,0,0][key=value]'),
+                       { object: 'node',
+                         bbox: {left:0,bottom:0,right:0,top:0},
+                         tag: {key: ['key'], value: ['value']} });
+        test.finish();
+    }
 };
 
 if (module === require.main) {
