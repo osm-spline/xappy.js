7e5fb8d754f980b9637868ed65b84c7c4039ee45
Alexander Sulfrian <alexander@sulfrian.net>
2011-05-05 23:57:24 +0200

    postgresdb: refactored to loop, changed indentation to spaces

--- src/nodejs/postgresdb/postgresdb.js
+++ src/nodejs/postgresdb/postgresdb.js
@@ -2,7 +2,7 @@ var pg = require('pg');
 var QueryBuilder = require('./querybuilder').QueryBuilder;
 
 var PostgresDb = function(connectionString) {
-	this.connectionString = connectionString;
+    this.connectionString = connectionString;
 };
 
 var rowToNode = function(row) {
@@ -14,58 +14,59 @@ var rowToWay = function(row) {
 var rowToRelation = function(row) {
 };
 
+var rowToObject = function(type, row) {
+    var objects = {
+        node: rowToNode,
+        way: rowToWay,
+        relation: rowToRelation
+    };
+
+    if (objects.hasOnwProperty(type)) {
+        objects[type](row);
+    }
+};
+
+var handleQuery = function(client, queryPlan, type, eventEmitter) {
+    var query = client.query(queryPlan);
+    query.on('row', function(row) {
+	eventEmitter.emit(type, rowToObject(type, row));
+    });
+
+    query.on('error', function(error) {
+	eventEmitter.emit('error', error);
+    });
+};
+
 //TODO change callback to return error?
 PostgresDb.prototype.executeRequest = function(xapiRequest, callback) {
-	var queryBuilder = new QueryBuilder();
-	var queryPlan = queryBuilder.createQueryPlan(xapiRequest);
+    var queryBuilder = new QueryBuilder();
+    var queryPlan = queryBuilder.createQueryPlan(xapiRequest);
 
-	//request client connection from the pg_pool
-	pg.connect(this.connectionString, function(error, client) {
-		if(error) {
-			//TODO log error
-			callback(error, null);
-		}
-		else {
-			var dbEventEmitter = new events.EventEmitter();
-			//TODO add on.error for every subquery; use for loop?
-			if(queryPlan.nodes) {
-				var nodesQuery = client.query(queryPlan.nodes);
-				nodesQuery.on('row', function(row) {
-					dbEventEmitter.emit('node',rowToNode(row));
-				});
-				nodesQuery.on('error', function(error) {
-					dbEventEmitter.emit('error', error);
-				});
-			}
-			if(queryPlan.ways) {
-				var waysQuery = client.query(queryPlan.ways);
-				waysQuery.on('row', function(row) {
-					dbEventEmitter.emit('way',rowToWay(row));
-				});
-				waysQuery.on('error', function(error) {
-					dbEventEmitter.emit('error', error);
-				});
-			}
-			if(queryPlan.relations) {
-				var relationsQuery = client.query(queryPlan.relations);
-				relationsQuery.on('row', function(row) {
-					dbEventEmitter.emit('relation',rowToRelation(row));
-				});
-				relationsQuery.on('error', function(error) {
-					dbEventEmitter.emit('error', error);
-				});
-			}
-			client.on('error', function(error) {
-				//TODO log error
-				dbEventEmmiter.emit('error', error);
-			});
-			client.on('drain', function() {
-				dbEventEmitter.emit('end');
-			});
+    //request client connection from the pg_pool
+    pg.connect(this.connectionString, function(error, client) {
+	if(error) {
+	    //TODO log error
+	    callback(error, null);
+	}
+	else {
+	    var dbEventEmitter = new events.EventEmitter();
+	    client.on('error', function(error) {
+		dbEventEmmiter.emit('error', error);
+	    });
+	    client.on('drain', function() {
+		dbEventEmitter.emit('end');
+	    });
 
-			callback(null, dbEventEmitter);
-		}
-	});
+            var type;
+            for (type in queryPlan) {
+                if (queryPlan.hasOwnProperty(type)) {
+                    handleQuerry(client, queryPlan[type], type, dbEventEmitter);
+                }
+            }
+
+	    callback(null, dbEventEmitter);
+	}
+    });
 };
 
 exports.PostgresDb = PostgresDb;
