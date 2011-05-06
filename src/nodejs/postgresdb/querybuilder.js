6d72c1ffb05088b2355ac52267d0a00d33d436bf
booo <borgers@mi.fu-berlin.de>
2011-05-06 17:32:09 +0200

    querybuilder: changed queryPlan layout

--- src/nodejs/postgresdb/querybuilder.js
+++ src/nodejs/postgresdb/querybuilder.js
@@ -5,7 +5,7 @@ QueryBuilder.prototype.createQueryPlan = function(xapiQueryObject) {
 	var queryPlan;
 	if(xapiQueryObject.object === 'node') {
 		queryPlan = {
-						'nodes' : {
+						'node' : {
 									'name' : '',
 									'text' : 'SELECT nodes.id, nodes.version, nodes.user_id, users.name AS user_name, nodes.tstamp, nodes.changeset_id, hstore_to_array(nodes.tags) AS tags, X(nodes.geom) AS lat, Y(nodes.geom) AS lon FROM nodes, users WHERE nodes.user_id = users.id;',
 									'values' : []
