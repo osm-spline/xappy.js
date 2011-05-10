662aeceb3d85a71479b425dcffc413bc360f80e3
yves <steve.harrison@gmx.net>
2011-05-10 11:54:06 +0200

    comments and fixes to main.js

--- src/nodejs/main.js
+++ src/nodejs/main.js
@@ -11,32 +11,11 @@ var parser = require('./requestParser');
 var config;
 var database;
 
-// #################### MAY be put to different module later
-//TODO move into xmlGenerator
-function toISO8601(date) {
-    //2007-03-31T00:09:22+01:00
-    var pad_two = function(n) {
-        return (n < 10 ? '0' : '') + n;
-    };
-
-    return [
-        date.getUTCFullYear(),
-        '-',
-        pad_two(date.getUTCMonth() + 1),
-        '-',
-        pad_two(date.getUTCDate()),
-        'T',
-        pad_two(date.getUTCHours()),
-        ':',
-        pad_two(date.getUTCMinutes()),
-        ':',
-        pad_two(date.getUTCSeconds()),
-        '+01:00'    //FIX ME
-            ].join('');
-}
-
-// #################### MAY be put to different module later
-
+/**
+ * Array of possible options values that are exxpeted by the application
+ *
+ * @type array
+ */
 var options = [
       { short       : 'c',
         long        : 'config',
@@ -45,52 +24,63 @@ var options = [
       }
 ];
 
-//
-// {
-// object = node/way/relation/* ,
-// bbox = { left : 1.0 , right : 1.0 , top : 1.0, bottom : 1.0 }
-// tag = { key : [ ], value [ ] }
-// }
 
+/**
+ * Main http handler, is called at every client request by node http server.
+ *
+ * @param req   node request object
+ * @param res   node response object
+ * 
+ */
 var httpHandler = function(req, res) {
     // FIXIT: if send head with 200, the following endWith500() call has no effect
     res.writeHead(200);
 
-    try {
-        var reqObj = parser.parse(req.url);
+    parser.parse(req.url, function requestParserCallback(err, xapiRequestObj) {
+        
+        if(error){
+            log.error(error);
+            res.writeHead(500);
+            res.end();
+        }   
 
-		database.executeRequest(reqObj, function(error, eventEmitter) {
-			if(error) {
-				log.error(error);
-				res.writeHead(500);
-			}
-			else {
-				//connection successful
-				//TODO check for first event -> send xmlHeader to client, perhaps put this into RepsonseXml?
-				var responseHandler = osmRes.mkXmlRe(res); //TODO rename responseHandler?
+        database.executeRequest(xapiRequestObj, function(error, eventEmitter) {
+            if(error) {
+                log.error(error);
+                res.writeHead(500);
+                res.end();
+            }
+            else {
+                //connection successful
+                //TODO check for first event -> send xmlHeader to client, perhaps put this into RepsonseXml?
+                var responseHandler = osmRes.mkXmlRe(res); //TODO rename responseHandler?
 
-				eventEmitter.on('node', function(node) {
-					responseHandler.putNode(node);
-				});
-				eventEmitter.on('way', function(way) {
-					responseHandler.putWay(way);
-				});
-				eventEmitter.on('relation', function(relation) {
-					responseHandler.putRelation(relation);
-				});
-				eventEmitter.on('error', function(error) {
-					res.write(500);
-				});
-				eventEmitter.on('end', function() {
-					responseHandler.finish();
-				});
-			}
-		});
-    }
-    catch (err) {
-        log.error(err);
-    }
+                eventEmitter.on('node', function(node) {
+                    responseHandler.putNode(node);
+                });
+                eventEmitter.on('way', function(way) {
+                    responseHandler.putWay(way);
+                });
+                eventEmitter.on('relation', function(relation) {
+                    responseHandler.putRelation(relation);
+                });
+                eventEmitter.on('error', function(error) {
+                    res.write(500);
+                });
+                eventEmitter.on('end', function() {
+                    responseHandler.finish();
+                });
+            }
+        });
+    });
 };
+
+/**
+ * Function that loads config file from specified path 
+*
+* @parameter configPath    either relative or absolut path
+ * @parameter callback      callback with error parameter and dict containing parsed options
+ */
 
 function getConfig(configPath, callback) {
     if( configPath[0] != '/'){
@@ -104,18 +94,23 @@ function getConfig(configPath, callback) {
     });
 }
 
+/**
+ * TODO: describe please!
+ * @parameter newConfig 
+ */
 function init(newConfig) {
     config = newConfig;
     xmlGenerator.config = config;
     log.setLevel(config.logLevel);
-    log.info("server starting...");
-    log.info("loaded config from " + configPath);
+    log.info('server starting...');
+    log.info('loaded config from ' + configPath);
 	database = new require('./postgresdb/postgresdb').PostgresDb();
     http.createServer(httpHandler).listen(config.port, config.host);
-    log.info("Started server at " + config.host + ":" + config.port );
+    log.info('Started server at ' + config.host + ':' + config.port );
 }
 
+// parse options, results can be retrived via opts.get('name') 
 opts.parse(options, true);
-configPath = opts.get('config') || "config.json";
-console.log("loading config " + configPath);
+configPath = opts.get('config') || 'config.json';
+console.log('loading config ' + configPath);
 config = getConfig(configPath, init);
