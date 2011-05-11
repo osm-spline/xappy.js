087e6bdc9089b22b7e87c09943e62ea479ef3293
booo <borgers@mi.fu-berlin.de>
2011-05-11 20:11:37 +0200

    helper-datatypes: please use jslint. Use underscore for loops?

--- src/nodejs/tests/helpers/helper-datatypes.js
+++ src/nodejs/tests/helpers/helper-datatypes.js
@@ -23,21 +23,21 @@ var knownDatatypes = {
             right : 'number',
             top : 'number',
             bottom : 'number'
-        },
+        }
     },
     requestTag : {
         type : 'costum',
         members: {
             key : 'listString',
-            value : 'listString',
-        },
+            value : 'listString'
+        }
     },
     listString : {
         type : 'list',
-        valueType : 'string',
+        valueType : 'string'
     }
 
-}
+};
 
 
 
@@ -56,24 +56,24 @@ var testObj = {
             left : 3.1,
             bottom: 4,
             right: 100,
-            top: 50 
+            top: 50
     },
     tag: {
             key: [ "adsd" ],
-            value: [ "asdas"] 
+            value: [ "asdas"]
     }
-}
+};
 
 
 module.exports.test = function asyncTest(test){
     var checkType = function(obj,type){
         console.log(obj + " " + type);
         // handle built primitiv datatypes
-        primitives = ['number','string']
+        primitives = ['number','string'];
             if ( primitives[type] ) {
-                test.ok(true)
+                test.ok(true);
             }
-        
+
         // handle types from knownTypes array
         if ( type.type === 'list' ) {
             for ( element in obj ) {
@@ -88,42 +88,42 @@ module.exports.test = function asyncTest(test){
         if ( type.type === 'costum' ) {
             checkCostumType(obj,type);
         }
-    }
+    };
 
     var checkEnumType = function(obj,type) {
 
         for ( value in type.values ) {
-            return;       
+            return;
         }
 
-    }
+    };
 
     var checkCostumType = function(obj,type){
 
         var members = knownDatatypes[type].members;
         var optionalMembers = knownDatatypes[type].optionalMembers;
 
-        for ( member in members ) {   
+        for ( member in members ) {
             test.ok(obj.hasOwnProperty(member),'Object is missing non optional property ' + member);
             checkType(obj[member],members[member]);
         }
 
-        for ( member in optionalMembers ) {   
+        for ( member in optionalMembers ) {
             if ( obj.hasOwnProperty(member) ) {
                 checkType(obj[member],optionalMembers[member]);
             }
         }
 
         return true;
-    }
-    
+    };
+
     setTimeout(function() {
     // make an assertion (these are just regular Node assertions)
     checkType(testObj,'xapiRequest');
     // finish the test
     test.finish();
   },0.001);
-}
+};
 
 async_testing.run(__filename, process.ARGV);
 
