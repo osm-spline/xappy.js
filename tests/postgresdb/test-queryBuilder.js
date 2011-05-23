ad8c5214ae7babde5047490ded03c622bfe696f2
Maiky <mareike.ziese@gmx.net>
2011-05-23 21:26:41 +0200

    extended querybuilder with ways(bboxes,tags)

--- tests/postgresdb/test-queryBuilder.js
+++ tests/postgresdb/test-queryBuilder.js
@@ -103,7 +103,7 @@ module.exports = {
                 var expected = {
                         node : {
                                 name : '',
-                                text :"SELECT * FROM nodes WHERE (linestring && st_setsrid(st_makebox2d(st_setsrid(st_makepoint($1, $2),4326)),st_srid(st_makepoint($3, $4),4326)),4326));",
+                                text :"SELECT * FROM nodes WHERE (ST_intersects(nodes.geom,4236), st_setsrid(st_makebox2d(st_setsrid(st_makepoint($1, $2),4326)),st_srid(st_makepoint($3, $4),4326)),4326)));",
                                 values : [13, 52, 15, 54],
                               	binary : true
                         }
@@ -180,7 +180,7 @@ module.exports = {
                 var expected = {
                         node : {
                                 name : '',
-                                text : "SELECT * FROM nodes WHERE (linestring && st_setsrid(st_makebox2d(st_setsrid(st_makepoint($1, $2),4326)),st_srid(st_makepoint($3, $4),4326)),4326)) AND (nodes.tags @> hstore($5,$7) OR nodes.tags @> hstore($5,$8) OR nodes.tags @> hstore($6,$7) OR nodes.tags @> hstore($6,$8));",
+                                text : "SELECT * FROM nodes WHERE (ST_intersects(nodes.geom,4236), st_setsrid(st_makebox2d(st_setsrid(st_makepoint($1, $2),4326)),st_srid(st_makepoint($3, $4),4326)),4326))) AND (nodes.tags @> hstore($5,$7) OR nodes.tags @> hstore($5,$8) OR nodes.tags @> hstore($6,$7) OR nodes.tags @> hstore($6,$8));",
                                 values : [13,52,15,54,'amenity','test','pub','val'],
                               binary : true,
                         }
@@ -189,5 +189,130 @@ module.exports = {
                 test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
                 test.finish();
         },
+
+        '//api/0.6/way[bbox=left,bottom,right,top]': function(test) {
+                //select all nodes whích are in the bbox
+                //queryBuilder should return an array of sql requests
+                var myQueryObject = {
+                        object : 'way',
+                        bbox : { left : 13,
+				 bottom : 52,
+				 right : 15,
+				 top : 54,
+		        }
+                };
+ 
+                var expected = {
+                        node : {
+                                name : '',
+                                text :"SELECT * FROM ways WHERE (ST_Crosses(st_setsrid(ways.geom,4326), st_setsrid(st_makebox2d(st_setsrid(st_makepoint($1, $2),4326)),st_srid(st_makepoint($3, $4),4326)),4326)));",
+                                values : [13, 52, 15, 54],
+                              	binary : true
+                        }
+                };
+                var input = new QueryBuilder().createQueryPlan(myQueryObject);
+                test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
+                test.finish();
+        },
+
+	'//api/0.6/way[tag=key:value]': function(test) {
+                //select all nodes whích are in the bbox
+                //queryBuilder should return an array of sql requests
+                var myQueryObject = {
+                        object : 'way',
+			tag : {
+                        	key : ['amenity'],
+				value : ['pub']
+			} 
+                };
+ 
+                var expected = {
+                        node : {
+                                name : '',
+                                text : "SELECT * FROM ways WHERE (ways.tags @> hstore($1,$2));",
+                                values : ['amenity','pub'],
+                              binary : true,
+                        }
+                };
+                var input = new QueryBuilder().createQueryPlan(myQueryObject);
+                test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
+                test.finish();
+        },
+
+	'//api/0.6/way[tag=key,key:value,value]': function(test) {
+                //select all nodes whích are in the bbox
+                //queryBuilder should return an array of sql requests
+                var myQueryObject = {
+                        object : 'way',
+			tag : {
+                        	key : ['amenity','test'],
+				value : ['pub','val']
+			} 
+                };
+ 
+                var expected = {
+                        node : {
+                                name : '',
+                                text : "SELECT * FROM ways WHERE (ways.tags @> hstore($1,$3) OR ways.tags @> hstore($1,$4) OR ways.tags @> hstore($2,$3) OR ways.tags @> hstore($2,$4));",
+                                values : ['amenity','test','pub','val'],
+                              binary : true,
+                        }
+                };
+                var input = new QueryBuilder().createQueryPlan(myQueryObject);
+                test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
+                test.finish();
+        },
+
+	'//api/0.6/way[2tag,bbox]': function(test) {
+                //select all nodes whích are in the bbox
+                //queryBuilder should return an array of sql requests
+                var myQueryObject = {
+                        object : 'way',
+                        bbox : { left : 13,
+				 bottom : 52,
+				 right : 15,
+				 top : 54,
+		        },
+			tag : {
+                        	key : ['amenity','test'],
+				value : ['pub','val']
+			} 
+                };
+ 
+                var expected = {
+                        node : {
+                                name : '',
+                                text : "SELECT * FROM ways WHERE (ST_Crosses(st_setsrid(ways.geom,4326), st_setsrid(st_makebox2d(st_setsrid(st_makepoint($1, $2),4326)),st_srid(st_makepoint($3, $4),4326)),4326))) AND (ways.tags @> hstore($5,$7) OR ways.tags @> hstore($5,$8) OR ways.tags @> hstore($6,$7) OR ways.tags @> hstore($6,$8));",
+                                values : [13,52,15,54,'amenity','test','pub','val'],
+                              binary : true,
+                        }
+                };
+                var input = new QueryBuilder().createQueryPlan(myQueryObject);
+                test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
+                test.finish();
+        },
+
+
+	'//api/0.6/way': function(test) {
+                //select all nodes whích are in the bbox
+                //queryBuilder should return an array of sql requests
+                var myQueryObject = {
+                        object : 'way'
+                };
+ 
+                var expected = {
+                        node : {
+                                name : '',
+                                text : "SELECT ways.*, users.name AS user_name, hstore_to_array(ways.tags) AS tags, X(ways.geom) AS lat, Y(ways.geom) AS lon FROM ways, users WHERE ways.user_id = users.id;",
+                                values : [],
+                              binary : true,
+                        }
+                };
+                var input = new QueryBuilder().createQueryPlan(myQueryObject);
+                test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
+                test.finish();
+        },
+
+
 
 };
