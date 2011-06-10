a0dae197bb2afc8a1c2478621596a889576fe9ee
lusy <vaseva@mi.fu-berlin.de>
2011-06-10 11:13:43 +0200

    tests: test-validator updated - validates correct simple object

--- tests/test-validator.js
+++ tests/test-validator.js
@@ -12,8 +12,22 @@ module.exports = {
                 test.deepEqual(xapiRequestInput,xapiRequestOut);
                 test.finish();
         });
-
     }
+
+    //'validate valid bbox' : function(test){
+    //};
+
+    /*
+     'validate correct compound object'
+     'validate bbox out of range'
+     'validate bbox swapped values'
+     'validate tag predicate with child predicate no(tags)'
+     'validate tag predicate with child predicate tags'
+     'validate node object with child predicate node or no(node)'
+     'validate way object with child predicate way or no(way)'
+     'validate way object with child predicate node or no(node)'
+     */
+
 };
 
 if (module == require.main) {
