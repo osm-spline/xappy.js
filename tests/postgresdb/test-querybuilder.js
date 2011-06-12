604b6a37e2196e04e938d597ff0e25d1ee490fd9
booo <borgers@mi.fu-berlin.de>
2011-06-12 22:58:39 +0200

    test-postgresdb: changed test for querybuilder to reflect new modular structure

--- tests/postgresdb/test-querybuilder.js
+++ tests/postgresdb/test-querybuilder.js
@@ -9,7 +9,17 @@ var sinon = require('sinon');
 
 module.exports = {
     'create query for node' : function(test) {
-        queryBuilder.createQueryPlan()         
+        var nodeQueryBuilder = require('../../lib/postgresdb/nodequerybuilder');
+        var spy = sinon.spy(nodeQueryBuilder,'createQueryPlan');
+        queryBuilder.createQueryPlan(sampleObjects['node']);
+        test.ok(spy.called);
+        test.finish();
+    },
+    'create query for way' : function(test) {
+        var wayQueryBuilder = require('../../lib/postgresdb/wayquerybuilder');
+        var spy = sinon.spy(wayQueryBuilder,'createQueryPlan');
+        queryBuilder.createQueryPlan(sampleObjects['way']);
+        test.ok(spy.called);
         test.finish();
     }
 };
