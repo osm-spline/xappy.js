991e120f76498127437a6cbf6a71b21ccbecd7d6
booo <borgers@mi.fu-berlin.de>
2011-06-12 20:07:34 +0200

    test-postgresdb: first real test for way requests (api/0.6/way)

---
+++ tests/postgresdb/test-wayQueryBuilder.js
@@ -0,0 +1,129 @@
+var wayQueryBuilder = require('../../lib/postgresdb/wayQueryBuilder');
+var sampleObjects = require('../helpers/helper-sampleXapiRequestObjects');
+
+module.exports = {
+    'way': function(test) {
+        var expected = {
+             node : {
+                name : '',
+                text : "SELECT nodes.id, nodes.version, nodes.user_id, nodes.tstamp, nodes.changeset_id, hstore_to_array(nodes.tags) AS tags, users.name AS user_name, X(nodes.geom) AS lat, Y(nodes.geom) AS lon FROM nodes, users, (SELECT DISTINCT node_id FROM way_nodes) AS nodesOfWays WHERE nodesOfWays.node_id = nodes.id AND users.id = nodes.user_id;",
+                values : [],
+                binary : true
+             },
+             way : {
+                name : '',
+                text : 'SELECT ways.id, ways.version, ways.user_id, ways.changeset_id, ways.tstamp, ways.nodes, hstore_to_array(ways.tags) AS tags, users.name AS user_name FROM ways, users WHERE ways.user_id = users.id;',
+                values : [],
+                binary : true
+             }
+        };
+        var input = wayQueryBuilder.createQueryPlan(sampleObjects['way']);
+        test.deepEqual(input, expected, '\texpected: ' + JSON.stringify(expected) + '\n\treturned: '+ JSON.stringify(input));
+        test.finish();
+    }
+
+    /*,
+    'way[bbox=left,bottom,right,top]': function(test) {
+        var expected = {
+            node : {
+                name : '',
+                text :"SELECT * FROM ways WHERE (ST_Crosses(st_setsrid(ways.geom,4326), st_setsrid(st_makebox2d(st_setsrid(st_makepoint($1, $2),4326)),st_srid(st_makepoint($3, $4),4326)),4326)));",
+                values : [13, 52, 15, 54],
+                binary : true
+            },
+            way : {
+                name : '',
+                text : '',
+                values : [],
+                binary : true
+            }
+        };
+        test.finish();
+    },
+    'way[tag=key:value]': function(test) {
+                //select all nodes whích are in the bbox
+                //queryBuilder should return an array of sql requests
+                var myQueryObject = {
+                    object : 'way',
+                    tag : {
+                        key : ['amenity'],
+                        value : ['pub']
+                    }
+                };
+
+                var expected = {
+                        node : {
+                                name : '',
+                                text : "SELECT * FROM ways WHERE (ways.tags @> hstore($1,$2));",
+                                values : ['amenity','pub'],
+                              binary : true
+                        }
+                };
+                var input = new QueryBuilder().createQueryPlan(myQueryObject);
+                test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
+                test.finish();
+        },
+
+    'way[tag=key,key:value,value]': function(test) {
+                //select all nodes whích are in the bbox
+                //queryBuilder should return an array of sql requests
+                var myQueryObject = {
+                        object : 'way',
+                        tag : {
+                            key : ['amenity','test'],
+                            value : ['pub','val']
+                    }
+                };
+
+                var expected = {
+                        node : {
+                                name : '',
+                                text : "SELECT * FROM ways WHERE (ways.tags @> hstore($1,$3) OR ways.tags @> hstore($1,$4) OR ways.tags @> hstore($2,$3) OR ways.tags @> hstore($2,$4));",
+                                values : ['amenity','test','pub','val'],
+                              binary : true
+                        }
+                };
+                var input = new QueryBuilder().createQueryPlan(myQueryObject);
+                test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
+                test.finish();
+        },
+
+    'way[2tag,bbox]': function(test) {
+                //select all nodes whích are in the bbox
+                //queryBuilder should return an array of sql requests
+                var myQueryObject = {
+                        object : 'way',
+                        bbox : { left : 13,
+                 bottom : 52,
+                 right : 15,
+                 top : 54
+                },
+            tag : {
+                key : ['amenity','test'],
+                value : ['pub','val']
+            }
+                };
+
+                var expected = {
+                        node : {
+                                name : '',
+                                text : "SELECT * FROM ways WHERE (ST_Crosses(st_setsrid(ways.geom,4326), st_setsrid(st_makebox2d(st_setsrid(st_makepoint($1, $2),4326)),st_srid(st_makepoint($3, $4),4326)),4326))) AND (ways.tags @> hstore($5,$7) OR ways.tags @> hstore($5,$8) OR ways.tags @> hstore($6,$7) OR ways.tags @> hstore($6,$8));",
+                                values : [13,52,15,54,'amenity','test','pub','val'],
+                              binary : true
+                        }
+                };
+                var input = new QueryBuilder().createQueryPlan(myQueryObject);
+                test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
+                test.finish();
+        }
+*/
+
+
+
+
+};
+
+//make this test standalone
+if (module == require.main) {
+    require('async_testing').run(__filename, process.ARGV);
+}
