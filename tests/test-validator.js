3c26d55b85f2d15542b8b83d31caf400cf25c0bf
lusy <vaseva@mi.fu-berlin.de>
2011-06-14 22:19:49 +0200

    test-validator.js and validator.js modified : added test for swapped bbox

--- tests/test-validator.js
+++ tests/test-validator.js
@@ -46,13 +46,32 @@ module.exports = {
                 //what should be the format of an error: (code,message)?
                 test.equal(error.code, 400);
                 test.strictEqual(error.message, "Bbox out of range. Please input values for left and right [-180,180], for top and bottom [-90,90]");
+                test.deepEqual(xapiRequestOut, null);
                 test.finish();
 
+        });
+    },
+
+    'validate bbox swapped values' : function(test){
+        var xapiRequestInput = {
+            object : 'node',
+            bbox : {
+                left : 123.456,
+                right : 98.9842,
+                top : -19.394,
+                bottom : -87.59823
+            }
+        };
+
+        validator.validate(xapiRequestInput, function(error,xapiRequestOut){
+            test.equal(error.code, 400);
+            test.strictEqual(error.message, "Bbox has swapped values. Please beware that left <= right and bottom <= top");
+            test.deepEqual(xapiRequestOut, null);
+            test.finish();
         });
     }
     /*
      'validate correct compound object'
-     'validate bbox swapped values'
      'validate tag predicate with child predicate no(tags)'
      'validate tag predicate with child predicate tags'
      'validate node object with child predicate node or no(node)'
