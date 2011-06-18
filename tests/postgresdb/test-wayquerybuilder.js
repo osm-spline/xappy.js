a7461045cffd3a907a0a4a09eda0a67bb1cbc9e8
booo <borgers@mi.fu-berlin.de>
2011-06-18 16:05:20 +0200

    postgresdb: added two more tests for wayquerybuilder

--- tests/postgresdb/test-wayquerybuilder.js
+++ tests/postgresdb/test-wayquerybuilder.js
@@ -60,65 +60,48 @@ module.exports = {
         var input = wayQueryBuilder.createQueryPlan(sampleObjects['way[name=Strandweg]']);
         test.deepEqual(input, expected, '\texpected: ' + JSON.stringify(expected) + '\n\treturned: '+ JSON.stringify(input));
         test.finish();
-    }/*,
-
-    'way[tag=key,key:value,value]': function(test) {
-                //select all nodes whích are in the bbox
-                //queryBuilder should return an array of sql requests
-                var myQueryObject = {
-                        object : 'way',
-                        tag : {
-                            key : ['amenity','test'],
-                            value : ['pub','val']
-                    }
-                };
-
-                var expected = {
-                        node : {
-                                name : '',
-                                text : "SELECT * FROM ways WHERE (ways.tags @> hstore($1,$3) OR ways.tags @> hstore($1,$4) OR ways.tags @> hstore($2,$3) OR ways.tags @> hstore($2,$4));",
-                                values : ['amenity','test','pub','val'],
-                              binary : true
-                        }
-                };
-                var input = new QueryBuilder().createQueryPlan(myQueryObject);
-                test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
-                test.finish();
-        },
-
-    'way[2tag,bbox]': function(test) {
-                //select all nodes whích are in the bbox
-                //queryBuilder should return an array of sql requests
-                var myQueryObject = {
-                        object : 'way',
-                        bbox : { left : 13,
-                 bottom : 52,
-                 right : 15,
-                 top : 54
-                },
-            tag : {
-                key : ['amenity','test'],
-                value : ['pub','val']
-            }
-                };
-
-                var expected = {
-                        node : {
-                                name : '',
-                                text : "SELECT * FROM ways WHERE (ST_Crosses(st_setsrid(ways.geom,4326), st_setsrid(st_makebox2d(st_setsrid(st_makepoint($1, $2),4326)),st_srid(st_makepoint($3, $4),4326)),4326))) AND (ways.tags @> hstore($5,$7) OR ways.tags @> hstore($5,$8) OR ways.tags @> hstore($6,$7) OR ways.tags @> hstore($6,$8));",
-                                values : [13,52,15,54,'amenity','test','pub','val'],
-                              binary : true
-                        }
-                };
-                var input = new QueryBuilder().createQueryPlan(myQueryObject);
-                test.deepEqual(input, expected, 'queryPlan with on query for all nodes');
-                test.finish();
-        }
-*/
-
-
-
-
+    },
+    'way[name|name:de=Strandweg|Strandweg]': function(test) {
+        var tagCondition = '(ways.tags @> hstore($1, $2) OR ways.tags @> hstore($3, $4) OR ways.tags @> hstore($5, $6) OR ways.tags @> hstore($7, $8))';
+        var expected = {
+             node : {
+                 name : '',
+                 text : 'SELECT nodes.id, nodes.version, nodes.user_id, nodes.tstamp, nodes.changeset_id, hstore_to_array(nodes.tags) AS tags, X(nodes.geom) AS lat, Y(nodes.geom) AS lon, users.name AS user_name FROM nodes, users, (SELECT DISTINCT way_nodes.node_id FROM ways, way_nodes WHERE ways.id = way_nodes.way_id AND ' + tagCondition + ') AS nodesOfWays WHERE nodes.user_id = users.id AND nodesOfWays.node_id = nodes.id;',
+                 values : ['name', 'Strandweg','name','Strandweg','name:de','Strandweg','name:de','Strandweg'],
+                 binary : true
+             },
+             way : {
+                 name : '',
+                 text : 'SELECT ways.id, ways.version, ways.user_id, ways.tstamp, ways.changeset_id, hstore_to_array(ways.tags) AS tags, ways.nodes, users.name AS user_name FROM ways, users WHERE ways.user_id = users.id AND ' + tagCondition + ';',
+                 values : ['name', 'Strandweg','name','Strandweg','name:de','Strandweg','name:de','Strandweg'],
+                 binary : true
+             }
+        };
+        var input = wayQueryBuilder.createQueryPlan(sampleObjects['way[name|name:de=Strandweg|Strandweg]']);
+        test.deepEqual(input, expected, '\texpected: ' + JSON.stringify(expected) + '\n\treturned: '+ JSON.stringify(input));
+        test.finish();
+    },
+    'way[name|name:de=Strandweg|Strandweg][11,53,12,54]': function(test) {
+        var tagCondition = '(ways.tags @> hstore($5, $6) OR ways.tags @> hstore($7, $8) OR ways.tags @> hstore($9, $10) OR ways.tags @> hstore($11, $12))';
+        var spacialCondition = '(ST_Crosses(st_setsrid(ways.linestring,4326),st_setsrid(st_makebox2d(st_setsrid(st_makepoint($1, $2),4326),st_setsrid(st_makepoint($3, $4),4326)),4326)))';
+        var expected = {
+             node : {
+                 name : '',
+                 text : 'SELECT nodes.id, nodes.version, nodes.user_id, nodes.tstamp, nodes.changeset_id, hstore_to_array(nodes.tags) AS tags, X(nodes.geom) AS lat, Y(nodes.geom) AS lon, users.name AS user_name FROM nodes, users, (SELECT DISTINCT way_nodes.node_id FROM ways, way_nodes WHERE ways.id = way_nodes.way_id AND ' + spacialCondition + ' AND ' + tagCondition + ') AS nodesOfWays WHERE nodes.user_id = users.id AND nodesOfWays.node_id = nodes.id;',
+                 values : [11, 53, 12, 54, 'name', 'Strandweg','name','Strandweg','name:de','Strandweg','name:de','Strandweg'],
+                 binary : true
+             },
+             way : {
+                 name : '',
+                 text : 'SELECT ways.id, ways.version, ways.user_id, ways.tstamp, ways.changeset_id, hstore_to_array(ways.tags) AS tags, ways.nodes, users.name AS user_name FROM ways, users WHERE ways.user_id = users.id AND ' + spacialCondition + ' AND ' + tagCondition + ';',
+                 values : [11, 53, 12, 54, 'name', 'Strandweg','name','Strandweg','name:de','Strandweg','name:de','Strandweg'],
+                 binary : true
+             }
+        };
+        var input = wayQueryBuilder.createQueryPlan(sampleObjects['way[name|name:de=Strandweg|Strandweg][bbox=11,53,12,54]']);
+        test.deepEqual(input, expected, '\texpected: ' + JSON.stringify(expected) + '\n\treturned: '+ JSON.stringify(input));
+        test.finish();
+    }
 };
 
 //make this test standalone
