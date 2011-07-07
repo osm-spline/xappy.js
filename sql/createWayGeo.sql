634e36f33e3bfc9d75dcbf6e53f01056b2cd753c
Alexander Sulfrian <alexander@sulfrian.net>
2011-07-07 16:08:10 +0200

    sql: added update to generate linestrings for ways

---
+++ sql/createWayGeo.sql
@@ -0,0 +1,8 @@
+﻿UPDATE ways SET linestring = (
+       SELECT ST_LineFromMultiPoint(ST_Collect(n.geom)) FROM (
+              SELECT nodes.geom as geom FROM way_nodes
+	        JOIN nodes ON (way_nodes.node_id = nodes.id)
+	        WHERE way_nodes.way_id = ways.id
+                ORDER BY way_nodes.sequence_id
+              ) as n
+       );
