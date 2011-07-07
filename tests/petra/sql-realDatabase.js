9438721c2d4eaa04a4f850715685fbd1d13b8c65
Mark Engel <mark.c.engel@gmail.com>
2011-07-07 13:22:03 +0200

    added new tests to real database

--- tests/petra/sql-realDatabase.js
+++ tests/petra/sql-realDatabase.js
@@ -33,15 +33,22 @@ var tagEmblem = {key: ['building'], value: ['emblem'] };
 var tagHotel = {key: ['amenity'], value: ['hotel']};
 
 function countNumberOfNodes(emitter, cb) {
-    var count = 0;
-    emitter.on('node', function() { count++; });
-    emitter.on('end', function() { cb(count); });
+    var nodes = 0;
+    var ways = 0;
+    var relations = 0;
+    emitter.on('node', function() { nodes++; });
+    emitter.on('way', function() { ways++; });
+    emitter.on('relation', function() { relations++; });
+    emitter.on('end', function() { cb(nodes, ways, relations); });
 }
 
-function testForCount(request, count, test) {
+
+function testForCount(request, test, nodes, ways, relations) {
     test.db.executeRequest(request, function(error, emitter) {
-        countNumberOfNodes(emitter, function(res) {
-            test.equal(count, res);
+        countNumberOfNodes(emitter, function(n, w, r) {
+            test.equal(nodes, n, 'nodes: expected: ' + nodes + ' was: ' + n);
+            test.equal(ways, w, 'ways: expected: ' + ways + ' was: ' + w);
+            test.equal(relations, r, 'relations: expected: ' + relations + ' was: ' + r);
             test.finish();
         });
     });
@@ -77,37 +84,96 @@ var nodesErrorSuite = {
 var nodesCountSuite = {
     'nodes: all nodes (7 in db)': function(test) {
         var request = { object: 'node' };
-        testForCount(request, 7, test);
+        testForCount(request, test, 7, 0, 0);
     },
     'nodes: all nodes in a big bbox (should contain all nodes)': function(test) {
         var request = { object: 'node', bbox: allBbox };
-        testForCount(request, 7, test);
+        testForCount(request, test, 7, 0, 0);
     },
     'nodes: test empty bbox': function(test) {
         var request = { object: 'node', bbox: emptyBbox };
-        testForCount(request, 0, test);
+        testForCount(request, test, 0, 0, 0);
     },
     'nodes: all nodes with a special tag': function(test) {
         var request = { object: 'node', tag: tagHospital };
-        testForCount(request, 2, test);
+        testForCount(request, test, 2, 0, 0);
     },
     'nodes: all nodes with a tag and a bbox': function(test) {
         // should return id 7
         var request = {object: 'node', tag: tagHospital, bbox: bottomBbox };
-        testForCount(request, 1, test);
+        testForCount(request, test, 1, 0, 0);
     }
 };
 
+var wayCountSuite = {
+    'ways: all ways': function(test) {
+        var request = { object: 'way' };
+        testForCount(request, test, 6, 3, 0);
+    },
+    'ways: all bbox': function(test) {
+        var request = { object: 'way', bbox: allBbox };
+        testForCount(request, test, 6, 3, 0);
+    },
+    'ways: empty bbox': function(test) {
+        var request = {object: 'way', bbox: emptyBbox };
+        testForCount(request, test, 0, 0, 0);
+    }
+}
+
 var relationErrorSuite = {
-    'test for error while requesting all relations': function(test) {
+    'relations error: all': function(test) {
         var request = { object: 'relation' };
+        testForError(request, test);
+    },
+    'relations error: all in bbox': function(test) {
+        var request = { object: 'relation', bbox: allBbox };
+        testForError(request, test);
+    },
+    'relations and tag': function(test) {
+        var request = { object: 'relation', tag: tagHospital };
+        testForError(request, test);
+    },
+    'relations: tag and bbox': function(test) {
+        var request = { object: 'relation', bbox: allBbox, tag: tagHospital };
         testForError(request, test);
     }
 };
 
 var wayErrorSuite = {
-    'test for error while requesting all ways': function(test) {
+    'ways: all': function(test) {
         var request = { object: 'way' };
+        testForError(request, test);
+    },
+    'ways: bbox': function(test) {
+        var request = { object: 'way' };
+        testForError(request, test);
+    },
+    'ways: tags': function(test) {
+        var request = { object: 'way', bbox: allBbox };
+        testForError(request, test);
+    },
+    'ways: tags and bbox': function(test) {
+        var request = { object: 'way', bbox: allBbox, tag: tagHospital };
+        testForError(request, test);
+    }
+};
+
+
+var allErrorSuite = {
+    'all: all': function(test) {
+        var request = { object: '*' };
+        testForError(request, test);
+    },
+    'all: bbox': function(test) {
+        var request = { object: '*' };
+        testForError(request, test);
+    },
+    'all: tags': function(test) {
+        var request = { object: '*', bbox: allBbox };
+        testForError(request, test);
+    },
+    'all: tags and bbox': function(test) {
+        var request = { object: '*', bbox: allBbox, tag: tagHospital };
         testForError(request, test);
     }
 };
@@ -132,10 +198,16 @@ var teardown = function(test, finish) {
     finish();
 };
 
+// Error testing
 exports.nodesError = suiteUp(nodesErrorSuite);
-exports.nodesCount = suiteUp(nodesCountSuite);
-// exports.relationsError = suiteUp(relationErrorSuite);
+exports.relationsError = suiteUp(relationErrorSuite);
 exports.wayError = suiteUp(wayErrorSuite);
+// exports.allError = suiteUp(allErrorSuite);
+
+// Count testing
+exports.nodesCount = suiteUp(nodesCountSuite);
+exports.waysCount = suiteUp(wayCountSuite);
+
 
 if (module == require.main) {
     var testing = require('coverage_testing');
