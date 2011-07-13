8d69a3764ff22a900011cc4e0cff9296b46ca882
booo <borgers@mi.fu-berlin.de>
2011-07-13 19:17:15 +0200

    postgresdb: major code cleanup aka beautification of the code; TODO starquerybuilder (david?)

---
+++ tests/postgresdb/helper-common.js
@@ -0,0 +1,19 @@
+var NODE_COLUMNS = 'nodes.id, nodes.version, nodes.user_id, users.name ' +
+        'AS user_name, nodes.tstamp, nodes.changeset_id, hstore_to_array(nodes.tags) ' +
+        'AS tags, Y(nodes.geom) AS lat, X(nodes.geom) AS lon';
+
+var WAY_COLUMNS = 'ways.id, ways.version, ways.user_id, users.name ' +
+        'AS user_name, ways.tstamp, ways.changeset_id, hstore_to_array(ways.tags) '
+        'AS tags, ways.nodes';
+
+var RELATION_COLUMNS = 'relations.id, relations.version, relations.user_id, users.name ' +
+        'AS user_name, relations.tstamp, relations.changeset_id, hstore_to_array(relations.tags) '
+        'AS tags, ' +
+        'relation_members.member_id, relation_members.member_type, ' +
+        'relation_members.member_role, relation_members.sequence_id';
+
+module.exports = {
+    NODE_COLUMNS: NODE_COLUMNS,
+    WAY_COLUMNS: WAY_COLUMNS,
+    RELATION_COLUMNS: RELATION_COLUMNS
+}
