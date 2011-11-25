1414242a8a2ad1aa64129dd6bbe61c4befa1cbd7
yves <steve.harrison@gmx.net>
2011-11-25 18:25:50 +0100

    tests for request cancel feature in emitter

--- tests/postgresdb/test-postgresdb.js
+++ tests/postgresdb/test-postgresdb.js
@@ -92,8 +92,7 @@ module.exports = {
                     setTimeout(function() {
                         //emit error event for client
                         client.emit('error', "clientError");
-                    },110);
-                    setTimeout(function() {
+                    },110);setTimeout(function() {
                         //emit error event for query; perhaps this should be an own testcase?
                         queryEventEmitter.emit('error', "queryError");
                     },120);
@@ -116,5 +115,50 @@ module.exports = {
                 }
             });
         });
-    }
+    },
+    'test all request cancel related operations' : function(test){
+        var myQueryObject = {
+            object : 'node'
+        };
+        var query = {
+            on : sinon.spy(),
+            once : sinon.spy(),
+            removeAllListeners : sinon.spy(),
+        }
+        var client = {
+            once : sinon.spy(),
+            on : sinon.spy(),
+            query : sinon.stub(),
+            cancel : sinon.stub()
+
+        }
+        var backend = {
+            connect : function(connectionString, callback) {
+
+                client.query.returns(query);
+
+                callback(null,client);
+            }
+        };
+        var databaseModule = new PostgresDb('',backend);
+
+        databaseModule.executeRequest(myQueryObject,function(err,emitter){
+            test.equal(typeof emitter.cancel,'function');
+            test.equal(emitter._runningQueries.length,1);
+
+            emitter.cancel();
+            client.cancel.calledWith(emitter._runningQueries[0]);
+
+            for( i in query.once.args) {
+                if(query.once.args[i][0] == 'end'){
+                    query.once.args[i][1]();
+                }
+            }
+            test.equal(emitter._runningQueries.length,0);
+
+
+            test.finish();
+        });
+
+    },
 };
